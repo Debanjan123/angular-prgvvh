f6b4cbb046b6a8ef5592dc625ae77653935b93e2
Debanjan123 <debanjanmca123@gmail.com>
2020-01-05 22:32:13 +0530

    src fix

--- src/app/core/services/http-header.service.ts
+++ src/app/core/services/http-header.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { CommonService } from 'src//shared/services/common.service';
+import { CommonService } from '//shared/services/common.service';
 
 @Injectable()
 export class HttpClientHeader {
